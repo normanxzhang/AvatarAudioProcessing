90bf10885d1fe89822ca0fa610f8e18870a943ea
Kiruuuu <thomasyoo11@gmail.com>
2022-01-15 16:57:48 -0700

    button functionality and gui design

--- pg_init.py
+++ pg_init.py
@@ -11,46 +11,49 @@ window = None
 fps = 30
 
 
-bg = pg.Color(255,255, 255)
+bg = pg.Color(0, 255, 0)
 timer = pg.time.Clock()
 window = pg.display.set_mode((screen_width, screen_height))
 pg.display.set_caption("HackED")
+mouse_click = False
+
+
 
 class button():
-	def __init__(self, color, x,y,width,height, text=''):
-		self.color = color
-		self.x = x
-		self.y = y
-		self.width = width
-		self.height = height
-		self.text = text
+    def __init__(self, color, x,y,width,height, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
 
-	def draw(self,win,outline=None):
-		#Call this method to draw the button on the screen
-		if outline:
-			pg.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4),0)
+    def draw(self,win,outline=None):
+        #Call this method to draw the button on the screen
+        if outline:
+            pg.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4),0,border_radius=25)
 
-		pg.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0)
+        pg.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0, border_radius=25)
 
-		if self.text != '':
-			font = pg.font.SysFont('cambria', 60)
-			text = font.render(self.text, 1, (0,0,0))
-			win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+        if self.text != '':
+            font = pg.font.SysFont('cambria', 60)
+            text = font.render(self.text, 1, (0,0,0))
+            win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
 
-	def over(self,pos):
-		#Pos is the mouse position or a tuple of (x,y) coordinates
-		if pos[0] > self.x and pos[0] < self.x + self.width:
-			if pos[1] > self.y and pos[1] < self.y + self.height:
-				return True
+    def over(self,pos):
+        #Pos is the mouse position or a tuple of (x,y) coordinates
+        if pos[0] > self.x and pos[0] < self.x + self.width:
+            if pos[1] > self.y and pos[1] < self.y + self.height:
+                return True
 
-		return False
+        return False
 
 def draw_window():
     window.fill(bg)
-    sprite_1.draw(window,(0,0,0))
+    char_1.draw(window,(0,0,0))
 
 
-sprite_1 = button((0,255,0), 0, 475, 225, 100,'Sprite 1')
+char_1 = button((255,255,255), screen_width/2 - 125, screen_height/2 - 50, 250, 100,'Start')
 
 # Game loop
 end = False
@@ -65,15 +68,19 @@ while end == False:
         if (event.type == QUIT):
             end = True
 
-        if event.type == pg.MOUSEBUTTONDOWN:
-            if sprite_1.over(pos) == MOUSEBUTTONDOWN:
-                end = True   # select sprite 1
-        
+        if pg.mouse.get_pressed()[0]:
+            mouse_click = True
+        else:
+            if mouse_click == True:
+                print('click')
+                mouse_click = False
+
         if event.type == pg.MOUSEMOTION:
-            if sprite_1.over(pos):
-                sprite_1.color = (255,0,0)
+            if char_1.over(pos):
+                char_1.color = (255,0,0)
+  
             else:
-                sprite_1.color = (0,255,0)
+                char_1.color = (255,255,255)
 
         # Update screen
         pg.display.update()
