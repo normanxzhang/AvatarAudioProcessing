30d99456d4fbaf16e7e60fdad98d7db7d51b8cd8
Kiruuuu <thomasyoo11@gmail.com>
2022-01-15 19:10:14 -0700

    button disappears and loses functionality on mouse 1 down

--- pg_init.py
+++ pg_init.py
@@ -11,7 +11,7 @@ window = None
 fps = 30
 
 
-bg = pg.Color('#ABDEE6')
+bg = pg.Color(0,255,0)
 timer = pg.time.Clock()
 window = pg.display.set_mode((screen_width, screen_height))
 pg.display.set_caption("HackED")
@@ -59,10 +59,10 @@ class button():
 
 def draw_window():
     window.fill(bg)
-    char_1.draw(window,(0,0,0))
+    start_btn.draw(window,(0,0,0))
 
 
-char_1 = button('#FFC8A2', screen_width/2 - 125, screen_height/2 + 100, 250, 100,6,'Start')
+start_btn = button('#ABDEE6', screen_width/2 - 125, screen_height/2 + 100, 250, 100,6,'Start')
 
 # Game loop
 run = True
@@ -83,9 +83,9 @@ while run == True:
 
         if not flag:
             if pg.mouse.get_pressed()[0]:
-                if char_1.over(pos):
+                if start_btn.over(pos):
                     mouse_click = True
-                    char_1 = button('#FFC8A2', screen_width/2 - 125, screen_height/2 + 105, 250, 100,6,'Start')
+                    start_btn = button('#ABDEE6', screen_width/2 - 125, screen_height/2 + 105, 250, 100,6,'Start')
             else:
                 if mouse_click == True:
                     print('click')
@@ -93,11 +93,11 @@ while run == True:
                     flag = True
 
         if event.type == pg.MOUSEMOTION:
-            if char_1.over(pos):
-                char_1.color = ('#FFC8A2')
+            if start_btn.over(pos):
+                start_btn.color = ('#ABDEE6')
   
             else:
-                char_1.color = ('#FFC8A2')
+                start_btn.color = ('#ABDEE6')
 
         # Update screen
         pg.display.update()
