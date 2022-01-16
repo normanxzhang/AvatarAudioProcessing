4d25a3df5a8c0f0c296a54a01ea5a3cda7be822b
MahitV <mahitvyas@googlemail.com>
2022-01-15 17:12:50 -0700

    Test

--- sprite_sheet.py
+++ sprite_sheet.py
@@ -1,5 +1,21 @@
 import pygame
 
 class spritesheet:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, filepath ):
+        self.sheet = pygame.image.load(filepath).convert()
+
+#Load sprites from a specific loaction
+
+def imageLoc(self, shape, colour = None):
+
+    shape = pygame.Shape(shape)
+    sprite = pygame.surface(shape.size).convert()
+    sprite.blit(self.sheet, (0,0), shape)
+    if colour is not None:
+        if colour is -1:
+            colour = sprite.getLoc(0,0)
+
+
+
+def imagesLoc(self,shapes, colour = None):
+    return[self.imageLoc(shape,)]
